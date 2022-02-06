c5b1bcf76e8cce30c9d8f02527e3d62add7537dc
stdingit <96324021+stdingit@users.noreply.github.com>
2022-02-06 11:12:09 +0530

    Add files via upload

---
+++ action1.js
@@ -0,0 +1,21 @@
+function image1() {
+    document.body.style.backgroundImage ="url('https://www.sciencealert.com/images/2021-01/processed/moon_topic_1024.jpg')";
+    document.getElementById("myDiv").style.backgroundImage ="url('https://www.sciencealert.com/images/2021-01/processed/moon_topic_1024.jpg')";
+}
+
+function image2() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/grohealth/image/upload/f_auto,fl_lossy,q_auto/v1583920665/DCUK/Content/iStock-947314334-1000x600.jpg')";
+    document.getElementById("sun").style.backgroundImage ="url('https://res.cloudinary.com/grohealth/image/upload/f_auto,fl_lossy,q_auto/v1583920665/DCUK/Content/iStock-947314334-1000x600.jpg')";
+
+}
+
+function image3() {
+    document.body.style.backgroundImage = "url('https://www.collinsdictionary.com/images/full/snow_306991961.jpg')";
+    document.getElementById("snow").style.backgroundImage ="url(https://www.collinsdictionary.com/images/full/snow_306991961.jpg')";
+}
+
+function image4() {
+    document.body.style.backgroundImage = "url('https://i.ytimg.com/vi/3wFvFvVZkwY/maxresdefault.jpg')";
+    document.getElementById("rain").style.backgroundImage ="url('https://i.ytimg.com/vi/3wFvFvVZkwY/maxresdefault.jpg')";
+}
+
